7031cd6ef7be543daaf02e9178ff9ffb4a4edd1e
erik-bobinski <ebobinsk0@gmail.com>
2025-03-01 17:56:40 -0500

    ran prettier

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -2,32 +2,32 @@ import { Table } from "@chakra-ui/react";
 import { events } from "../data/seed";
 
 function EventsPage(): JSX.Element {
-    return (
-        <Table.Root size="sm" showColumnBorder>
-            <Table.Header>
-                <Table.Row>
-                    <Table.ColumnHeader>Name</Table.ColumnHeader>
-                    <Table.ColumnHeader>Organizer</Table.ColumnHeader>
-                    <Table.ColumnHeader>Date</Table.ColumnHeader>
-                    <Table.ColumnHeader>Location</Table.ColumnHeader>
-                    <Table.ColumnHeader>Budget</Table.ColumnHeader>
-                    <Table.ColumnHeader textAlign="end">Event Type</Table.ColumnHeader>
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {events.map((event) => (
-                    <Table.Row key={event.name}>
-                        <Table.Cell>{event.name}</Table.Cell>
-                        <Table.Cell>{event.organizer.name}</Table.Cell>
-                        <Table.Cell>{event.date}</Table.Cell>
-                        <Table.Cell>{event.location}</Table.Cell>
-                        <Table.Cell>{event.budget}</Table.Cell>
-                        <Table.Cell textAlign="end">{event.eventType}</Table.Cell>
-                    </Table.Row>
-                ))}
-            </Table.Body>
-        </Table.Root>
-    )
-};
+  return (
+    <Table.Root size="sm" showColumnBorder>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeader>Name</Table.ColumnHeader>
+          <Table.ColumnHeader>Organizer</Table.ColumnHeader>
+          <Table.ColumnHeader>Date</Table.ColumnHeader>
+          <Table.ColumnHeader>Location</Table.ColumnHeader>
+          <Table.ColumnHeader>Budget</Table.ColumnHeader>
+          <Table.ColumnHeader textAlign="end">Event Type</Table.ColumnHeader>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {events.map((event) => (
+          <Table.Row key={event.name}>
+            <Table.Cell>{event.name}</Table.Cell>
+            <Table.Cell>{event.organizer.name}</Table.Cell>
+            <Table.Cell>{event.date}</Table.Cell>
+            <Table.Cell>{event.location}</Table.Cell>
+            <Table.Cell>{event.budget}</Table.Cell>
+            <Table.Cell textAlign="end">{event.eventType}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+}
 
 export default EventsPage;
