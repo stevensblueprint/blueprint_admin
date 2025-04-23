54a4dcbcf319b6f80e42eadd826aa52f2f80de4e
miguel-merlin <mmerlin@stevens.edu>
2025-04-23 10:11:11 -0400

    deps: added mapstruc dependency

--- src/test/java/com/sitblueprint/admin/AdminApplicationTests.java
+++ src/test/java/com/sitblueprint/admin/AdminApplicationTests.java
@@ -1,13 +1,8 @@
 package com.sitblueprint.admin;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AdminApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
